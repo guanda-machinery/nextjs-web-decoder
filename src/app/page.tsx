c6acdfa8f830964d26973279cfe1758cc9d473c4
haneyume <haneyume@gmail.com>
2025-06-19 14:55:29 +0800

    feat: 新增條碼掃描資料狀態管理與顯示介面

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,38 @@
 "use client";
 
-import React from "react";
+import { useState, useEffect } from "react";
 
 import Reader from "@/features/FuncodeDecoder";
 
 export default function HomePage() {
+  const [scannedData, setScannedData] = useState<string>("");
+
+  useEffect(() => {
+    // TODO: send scannedData to server
+    console.log("🔥 use effect decode success =>", scannedData);
+  }, [scannedData]);
+
   return (
-    <div className="w-[300px] h-[300px]">
-      <Reader
-        delay={500}
-        onError={(e: any) => console.log("decode error =>", e)}
-        onScan={(data: any) => console.log("🔥 decode success =>", data)}
-        startupcode="guandamachinelocal"
-        debug
-      />
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <div className="w-[300px] h-[300px]">
+        <Reader
+          delay={500}
+          onError={(e: any) => console.log("onError =>", e)}
+          onScan={(data: any) => {
+            console.log("🔥 onScan =>", data);
+
+            const jsonData = JSON.parse(data);
+
+            if (jsonData.Barcodes.length > 0) {
+              setScannedData(jsonData.Barcodes[0].Content);
+            }
+          }}
+          startupcode="guandamachinelocal"
+          debug
+        />
+      </div>
+
+      <div className="text-lg font-bold">{scannedData || "No data"}</div>
     </div>
   );
 }
